fcc0084da65252d92e924b3c2c56092748faef81
aimee rivers <aimee.rivers@bbc.co.uk>
2015-05-21 16:50:38 +0100

    Add a custom script to create a brand and episodes.

---
+++ create_brand_and_episodes.rb
@@ -0,0 +1,50 @@
+require 'pips'
+
+PREFERENCES = {
+  brand_title: 'Store Rights applied at Brand Level',
+  rights_policy_pid: 'p00t4h2v'
+}
+
+def generate_on_air_id
+  (0...4).map{(65+rand(26)).chr}.join +
+    (0...3).map{rand(9).to_s}.join +
+    (65+rand(26)).chr
+end
+
+brand = PIPS::XML::Brand.new
+brand.save(title: PREFERENCES[:brand_title])
+
+puts "Brand: #{brand.pid}"
+
+rights_policy = PIPS::XML::RightsPolicy.new(pid: PREFERENCES[:rights_policy_pid])
+
+rights_contract = PIPS::XML::RightsContract.new
+rights_contract.save(
+  odm_managed: true,
+  rights_policy: rights_policy,
+  start_date: Time.now + 10,
+  brand: brand
+)
+
+puts "Rights Contract: #{rights_contract.pid}"
+
+30.times do |i|
+
+  on_air_id = generate_on_air_id
+
+  episode = PIPS::XML::Episode.new
+  episode.save(
+    title: "Episode #{i+1}",
+    presentation_title: "Episode #{i+1}",
+    other_ids: [{type: 'uid', authority: 'onair', value: on_air_id}],
+    brand: brand
+  )
+  puts "#{episode.title}: #{episode.pid}"
+
+  version = PIPS::XML::Version.new
+  version.save(
+    episode: episode,
+    other_ids: [{type: 'uid', authority: 'onair', value: "#{on_air_id}/01"}]
+  )
+
+end
